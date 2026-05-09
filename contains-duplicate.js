6e83f0db53fd839e481d0ceacb40814522604f51
sourov <sourovsh533@gmail.com>
2026-05-09 11:41:04 +0600

    solutions of contains-duplicates

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -1,2 +1,18 @@
 // problem link: https://neetcode.io/problems/duplicate-integer/question?list=neetcode150
 
+function containsDuplcate(nums) {
+    const seen = new Set();
+    
+
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+        console.log(seen);
+    }
+    return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 3])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
